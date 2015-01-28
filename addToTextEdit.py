f60124444ab598826f39edb82cbebb5a7c4c022f
ftwstork123 <jacob.rubenson@gmail.com>
2015-01-28 01:33:02 +0100

    test

--- addToTextEdit.py
+++ addToTextEdit.py
@@ -39,6 +39,7 @@ class Form(QDialog):
 			self.grid.addWidget(self.removeButtons[self.nCards],self.nCards+1,3)
 			self.removeButtons[self.nCards].clicked.connect(lambda: self.removeCard(nCard))
 			self.nCards = self.nCards+1
+			
 			self.setLayout(self.grid)
 	def removeCard(self,cardName): 
 		if int(self.counters[cardName].text()) > 0 : 
